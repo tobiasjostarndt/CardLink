302252cc16cc24453de3fcfe6bb684b5e8c7fa5b
tobiasjostarndt <69247921+tobiasjostarndt@users.noreply.github.com>
2024-09-23 14:37:37 +0200

    Add files via upload

--- www/CardLink.js
+++ www/CardLink.js
@@ -5,6 +5,9 @@ var PLUGIN_NAME = 'CardLink';
 var CardLink = {
     establishWSS: function (arg0, cb) {
         exec(cb, null, PLUGIN_NAME, 'establishWSS', [arg0]);
+    },
+    isConnected: function(cb){
+        exec(cb, null, PLUGIN_NAME, 'isConnected', []);
     }
 };
 
