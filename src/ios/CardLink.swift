0e1a9572b33b4bf5e2c92a4458f780d21ed0c8b1
tobiasjostarndt <69247921+tobiasjostarndt@users.noreply.github.com>
2024-09-23 13:25:34 +0200

    Add files via upload

--- src/ios/CardLink.swift
+++ src/ios/CardLink.swift
@@ -1,5 +1,8 @@
 @objc(CardLink) class CardLink: CDVPlugin {
-
+    
+    // Verwende lazy var, um die Instanz beim ersten Zugriff zu erstellen
+    private lazy var webSocketClientManager = WebSocketClientManager()
+    
     @objc(establishWSS:)
     func establishWSS(command: CDVInvokedUrlCommand) {
         var pluginResult: CDVPluginResult? = nil
@@ -7,24 +10,27 @@
         // Überprüfen, ob der erste Parameter eine gültige URL ist
         if let wssURL = command.arguments.first as? String, !wssURL.isEmpty {
             // Verbindung mit der URL herstellen
-            //self.webSocketClientManager.connectToURL(wssURL)
-            
-            // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
-            //if self.webSocketClientManager.isConnected {
-                //pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "true")
-            //} else {
-                //pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-            //}
-
-            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: wssURL)
+            self.webSocketClientManager.connect(to: wssURL)
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "true")
         } else {
             pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
         }
-
+        
         // Das Ergebnis an den Cordova-Callback zurückgeben
         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
-    
-    // WebSocketClientManager-Instanz bereitstellen
-    private var webSocketClientManager = WebSocketClientManager()
+
+    @objc(isConnected:)
+    func isConnected(command: CDVInvokedUrlCommand) {
+        var pluginResult: CDVPluginResult? = nil
+
+        if self.webSocketClientManager.isConnected {
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "true")
+        } else {
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "false")
+        }
+        
+        // Das Ergebnis an den Cordova-Callback zurückgeben
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
 }
